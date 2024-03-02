93c5de7901286d064d4f1f76065af42670916170
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-03-02 12:57:51 +0530

    react-bootstrap configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
      <h1>Sticky notes</h1>
+     <Button>Button</Button>
     </div>
   );
 }
